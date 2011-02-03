992bdbc012949c1ca71fe5d51c1493d9b0f6aaec
Edijs Petersons <eddpeterson@gmail.com>
2011-02-03 23:03:30 +0100

    Added mocks and stubs. Finished on http://kerryb.github.com/iprug-rspec-presentation/#57

---
+++ mocks_and_sutbs_spec.rb
@@ -0,0 +1,46 @@
+class MocksAndSutbsSpec1
+  def valid?
+    false
+  end
+  
+  def find criteria
+    "1"
+  end
+end
+
+
+
+describe MocksAndSutbsSpec1 do
+  let(:myObject) { MocksAndSutbsSpec1.new }
+                   
+  it "should be valid" do
+    myObject.stub(:valid?).and_return true
+    myObject.valid?.should be_true
+  end
+  
+  it "should find 2" do
+    myObject.stub(:find).with(2).and_return 2
+    result = myObject.find 2
+    result.should == 2
+  end
+  
+  #Successive calls can return different values:
+  #foo.stub(:bar).and_return 1, 2, 3
+  #foo.bar
+    # => 1
+  #foo.bar
+    # => 2
+  #foo.bar
+    # => 3
+  #foo.bar
+    # => 3
+  
+  
+  
+  
+  
+end
+
+
+
+
